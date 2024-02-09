ebc3b18132a35278b518aad46a09bbd3de3da411
saharDarvishian <darvishiansahar@gmail.com>
2024-02-09 12:44:09 +0330

    add-js-classes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 import { fetchData } from "./utils/httpReq.js";
+import Products from "./models/Products.js";
+import Cart from "./models/Cart.js";
+
+const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document
+  .getElementById("total-price")
+  .querySelector("span");
 
 async function render() {
   const productsData = await fetchData();
-  console.log(productsData);
+
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productsInstance = new Products(
+    productsNode,
+    productsData,
+    cartInstance
+  );
+  productsInstance.showProducts();
 }
 
 document.addEventListener("DOMContentLoaded", render);
---
+++ models/Products.js
@@ -0,0 +1,57 @@
+class Products {
+  constructor(parent, products, cart) {
+    this.parent = parent;
+    this.products = products;
+    this.cart = cart;
+    this.parent.addEventListener("click", this);
+  }
+
+  showProducts() {
+    this.products.forEach((product) => this.creatCard(product));
+  }
+
+  creatCard(data) {
+    const cardEle = document.createElement("div");
+    const imgEle = this.productImg(data);
+    const infoEle = this.productInfo(data);
+
+    cardEle.innerHTML = imgEle;
+    cardEle.innerHTML += infoEle;
+    this.parent.append(cardEle);
+  }
+
+  productImg(data) {
+    const { image, alt } = data;
+    const imgJSX = `<img alt=${alt} src=${image} />`;
+    return imgJSX;
+  }
+
+  productInfo(data) {
+    const { id, name, price } = data;
+    const infoJSX = `
+      <div id="product-info">
+          <h3>${name}</h3>
+          <div>
+              <span>${price}</span>
+              <button data-id=${id}>+</button>
+          </div> 
+      </div>
+`;
+    return infoJSX;
+  }
+
+  handleEvent() {
+    const element = event.target;
+    if (element.tagName === "BUTTON") {
+      this.addToCart(element.dataset.id);
+    }
+  }
+
+  addToCart(id) {
+    const product = this.products.find((i) => i.id === +id);
+    this.cart.products.push(product);
+    console.log(this.cart);
+  }
+}
+
+export default Products;
